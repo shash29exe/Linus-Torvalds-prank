8e97937084e63e9e7122093f3ac41714dccd4415
shash29 <zzxc67643@gmail.com>
2025-07-01 18:50:00 +0500

    fullscreen mode, play audio

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 function removeOverlay() {
-    var overlay = document.getElementById("overlay");
-    overlay.style.display = "none";
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('video-container').style.display = 'block';
+
+    const videoContainer = document.getElementById('video-container');
+
+    if (videoContainer.requestFullscreen) {
+        videoContainer.requestFullscreen();
+    } else if (videoContainer.webkitRequestFullscreen) {
+        videoContainer.webkitRequestFullscreen();
+    } else if (videoContainer.msRequestFullscreen) {
+        videoContainer.msRequestFullscreen();
+    }
 }
+        
+function playAudio() {
+    var audio = new Audio('../media/audio.mp3');
+    audio.play();
+}
+        
+document.addEventListener('fullscreenchange', (event) => {
+    if (!document.fullscreenElement) {
+        if (videoContainer.requestFullscreen) {
+            videoContainer.requestFullscreen();
+        }
+    }
+});
