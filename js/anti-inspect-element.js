b358cd3cba547e5844d3e4896ab72dbdd744c8d2
shash29 <zzxc67643@gmail.com>
2025-07-01 13:51:40 +0500

    Create anti-inspect-element.js

---
+++ js/anti-inspect-element.js
@@ -0,0 +1,41 @@
+/**
+    * @author san randry
+    * @license MIT license
+ * this plugin disable all inspect element feature on a web browser
+*/
+// disable right click
+document.oncontextmenu = function (e) {
+    e.preventDefault()
+    return false;
+}
+ // disable some additional keys
+document.onkeydown = function (e) {
+    // disable f12
+    if (event.keyCode == 123) {
+        return false;
+    }
+    // disable f11
+    if (event.keyCode == 122) {
+        return false;
+    }
+    // disable ctrl+shift+I
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    // disable ctrl+shift+C
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    // disable ctrl+shift+j
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    // disable ctrl+shift+U
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+    // disable ESC (keyCode 27)
+    if (e.keyCode == 27) {
+        return false;
+    }
+}
